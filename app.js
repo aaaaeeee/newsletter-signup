5bfc930a6cf9b7198230109af9e3bae31addb2ad
aaaaeeee <36264276+aaaaeeee@users.noreply.github.com>
2018-03-13 14:32:40 +0200

    ver1.0

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const bodyParser = require('body-parser');
+const request = require("request");
 
-//app.use express.static(__dirname + '/public')
-app.use(express.static(path.join(__dirname,'public')));
-app.listen(3000,function () {
+//Middleware
+app.use(express.static(__dirname + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+app.listen(3000, function() {
   console.log("port 3000");
 });
 
-app.post('/', function(req,res){
+app.post('/', function(req, res) {
+  addEmailToMailchimp(req.body.email);
+  console.log(req.body.email);
   res.end('success');
 });
+
+function addEmailToMailchimp(email) {
+  var options = {
+    method: 'POST',
+    url: 'https://us12.api.mailchimp.com/3.0/lists/<YOUR LIST ID>/members',
+    headers: {
+      'Postman-Token': '275a3cef-fde0-41ff-bbee-df9b661507bc',
+      'Cache-Control': 'no-cache',
+      Authorization: 'Basic YW55c3RyaW5nOjRmMDY1NGZiYzlkM2Y5ZTNlOWJjNzRkZTUxMDA3ZDYxLXVzMTI=',
+      'Content-Type': 'application/json'
+    },
+    body:{
+      email_address:email,
+      status: 'subscribed'
+    },
+    json:true
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+
+};
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,14 @@
 var $ = require('jquery');
+
 $('form').submit(function(event) {
+  var userEmail = $('#email').val();
+  console.log(userEmail);
   event.preventDefault(); // tama estaa normitoiminnan
   $.ajax({
     url: "/",
     type: 'POST',
     data: {
-      email:'test@test.com'
+      email:userEmail
     },
     success: function(response) {
       console.log(response);
